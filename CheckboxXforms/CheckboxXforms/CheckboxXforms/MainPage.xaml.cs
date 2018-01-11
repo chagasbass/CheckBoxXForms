b18d254144cbb485d11e9fd147e1e3a728e39986
Thiago Chagas <thiago.chagas@blnet.com>
2018-01-11 14:47:19 -0200

    Commit 1

---
+++ CheckboxXforms/CheckboxXforms/CheckboxXforms/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace CheckboxXforms
+{
+    public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+
+        public async void VerificarSelecao(object sender,EventArgs args)
+        {
+            var cb = (XLabs.Forms.Controls.CheckBox)sender;
+
+            var selecionado = cb.DefaultText;
+
+            await this.DisplayAlert("Atenção", "Opção selecionada: " + selecionado, "OK");
+        }
+    }
+}
